0b685fb6c1f68ae008acd6c2c49bcb4040c539a9
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-08-23 12:42:31 -0500

    Create Project01.java

---
+++ Project01.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class Project01 {
+
+  public static void main(String args[]) {
+
+    String name = JOptionPane.showInputDialog("What is your name?");
+
+    String hourlywage = JOptionPane.showInputDialog(name + ", how much do you make per hour?");
+    double hourlywagenum = Double.parseDouble(hourlywage);
+
+    String amthoursstr = JOptionPane.showInputDialog("One more question " + name + "; how many hours did you work last week?");
+    double amthoursnum = Double.parseDouble(amthoursstr);
+
+    double weekwage = amthoursnum * hourlywagenum;
+
+    JOptionPane.showMessageDialog(null, name + " made $" + weekwage + " last week.");
+    
+  }
+  
+}
