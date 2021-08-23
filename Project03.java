c1630460b2542860fa6f48c5853ab30baa02f0ab
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-08-23 12:43:26 -0500

    Create Project03.java

---
+++ Project03.java
@@ -0,0 +1,19 @@
+public class Project03 {
+    
+    static void printNcharacters(String str, int numCharacters) {
+      for (int i = 0; i < numCharacters; i++) {
+        System.out.print(str);  
+      }
+      System.out.println();
+    }
+    
+    
+    public static void main(String args[])
+    {
+       printNcharacters("@", 12);
+       printNcharacters("*", 3);
+       printNcharacters("$", 5);
+       printNcharacters("$@", 4);
+       printNcharacters("&@$%", 5);
+    }
+}
