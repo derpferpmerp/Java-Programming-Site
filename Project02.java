3470e01bf66de2511b85b157336d8a392d44d8be
derpferpmerp <47375701+derpferpmerp@users.noreply.github.com>
2021-08-23 12:43:00 -0500

    Create Project02.java

---
+++ Project02.java
@@ -0,0 +1,112 @@
+public class Project02 {
+		
+	public static void Str1() {System.out.println("  *");}
+	public static void Str2() {System.out.println(" * ");}
+	public static void Str3() {System.out.println("*  ");}
+	public static void Str4() {System.out.println(" **");}
+	public static void Str5() {System.out.println("** ");}
+	public static void Str6() {System.out.println("* *");}
+	public static void Str7() {System.out.println("***");}
+	public static void Str8() {System.out.println("   ");}
+	
+	public static void main(String[] args) {
+		// 1
+		Str1();
+		Str1();
+		Str1();
+		Str1();
+		Str1();
+		
+		Str8();  
+		Str8();	
+
+
+		// 2
+		Str7();
+		Str1();
+		Str7();
+		Str3();
+		Str7(); 
+
+		Str8();  
+		Str8();	
+
+		// 3
+
+		Str7();
+		Str1();
+		Str7();
+		Str1();
+		Str7();
+
+		Str8();  
+		Str8();	
+
+		// 4
+
+		Str6();
+		Str6();
+		Str7();
+		Str1();
+		Str1();
+
+		Str8();  
+		Str8();
+
+		// 5
+
+		Str7();
+		Str3();
+		Str7();
+		Str1();
+		Str7();
+
+		Str8();  
+		Str8();	
+
+		// 6
+
+		Str7();
+		Str3();
+		Str7();
+		Str6();
+		Str7();
+
+		Str8();  
+		Str8();	
+
+		// 7
+
+		Str7();
+		Str1();
+		Str1();
+		Str1();
+		Str1();
+
+		Str8();  
+		Str8();	
+
+		// 8
+
+		Str7();
+		Str6();
+		Str7();
+		Str6();
+		Str7();
+
+		Str8();  
+		Str8();	
+
+		// 9
+
+		Str7();
+		Str6();
+		Str7();
+		Str1();
+		Str1();
+
+		Str8();  
+		Str8();	
+	}
+  
+}
