bc1df6808f7ab1705794c10d24159f3d48f41668
derpferpmerp <cmfleming@eths202.org>
2021-09-02 20:49:30 -0500

    Finished Project 20

---
+++ Project20.java
@@ -0,0 +1,27 @@
+import java.text.MessageFormat;
+import java.util.*;
+
+import static java.util.stream.Collectors.toCollection;
+import static javax.swing.JOptionPane.*;
+
+public class Project20 {
+
+	public static List<Integer> genRandList(Integer amt) {
+		Random rng = new Random();
+		List<Integer> list = new ArrayList<>();
+		for (int i = 0; i < amt; i++) {
+			int integer = rng.nextInt(99) + 1;
+			list.add(integer);
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		int amountOfNumbers = Integer.parseInt(showInputDialog("How Many Numbers Do You Want to Sort?"));
+		List<Integer> randomNumbers = genRandList(amountOfNumbers);
+		List<String> randOut = randomNumbers.stream().map(String::valueOf).collect(toCollection(ArrayList::new));
+		System.out.println("Unsorted: " + randOut);
+		Collections.sort(randomNumbers);
+		System.out.println("Sorted: " + randomNumbers);
+	}
+}
