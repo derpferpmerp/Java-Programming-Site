89f3ec39fe82afd8659c0d76222e2e7bb51f52a8
derpferpmerp <cmfleming@eths202.org>
2021-12-08 23:02:37 -0600

    Fixed Project20

--- Project20.java
+++ Project20.java
@@ -4,26 +4,50 @@ import static javax.swing.JOptionPane.*;
 
 public class Project20 {
 
-	public static List<Integer> genRandList(Integer amt) {
+	public static int[] genRandList(int amt) {
 		Random rng = new Random();
-		List<Integer> list = new ArrayList<>();
+		int[] list = new int[amt];
 		for (int i = 0; i < amt; i++) {
 			int integer = rng.nextInt(99) + 1;
-			list.add(integer);
+			list[i] = integer;
 		}
 		return list;
 	}
 
+	public static String[] sortAsString(int[] arr) {
+		String[] ol = new String[arr.length];
+		int indxITR = 0;
+		while (arr.length != 0) {
+			int mx = 0, mxIDX = 0;
+			int[] newList = new int[arr.length-1];
+			for (int i = 0; i < arr.length; i++) {
+				int j = arr[i];
+				if (j > mx) {
+					mx = j;
+					mxIDX = i;
+				}
+			}
+			for (int i = 0; i < arr.length; i++) {
+				int g = (i < mxIDX) ? i : ((i > mxIDX) ? i-1 : -1);
+				if (Objects.equals(g,-1)) { continue; }
+				newList[g] = arr[i];
+			}
+			arr = newList;
+			ol[indxITR] = String.valueOf(mx);
+			indxITR += 1;
+		}
+		return ol;
+	}
+
 	public static void main(String[] args) {
 		int amountOfNumbers = Integer.parseInt(showInputDialog("How Many Numbers Do You Want to Sort?"));
-		List<Integer> randomNumbers = genRandList(amountOfNumbers);
-		List<String> randOut = new ArrayList<>();
-		for (Integer randomNumber : randomNumbers) {
-			String s = String.valueOf(randomNumber);
-			randOut.add(s);
+		int[] randomNumbers = genRandList(amountOfNumbers);
+		String[] randOut = new String[randomNumbers.length];
+		for (int i = 0; i < randomNumbers.length; i++) {
+			randOut[i] = String.valueOf(randomNumbers[i]);
 		}
-		System.out.println("Unsorted: " + randOut);
-		randomNumbers.sort(null);
-		System.out.println("Sorted: " + randomNumbers);
+		System.out.println("Unsorted: " + String.join(", ", randOut));
+		String[] sortedNumbers = sortAsString(randomNumbers);
+		System.out.println("Sorted: " + String.join(", ", sortedNumbers));
 	}
 }
